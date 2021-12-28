e90d6c3999b28441fa302fd1cc487db5c207d0d3
Odds <33238619+goblinodds@users.noreply.github.com>
2021-12-28 13:31:56 -0800

    test input

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
 // pasted from https://dev.to/programliftoff/create-a-basic-webpage-with-css-and-javascript--104i
 
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+var protag = window.prompt("Enter your name: ");
+alert("Your name is " + protag);
